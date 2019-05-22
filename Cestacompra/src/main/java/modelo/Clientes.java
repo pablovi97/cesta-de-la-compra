75496f15ca6cf58e8746035583448289979e7bc2
Pablo Viera <32093629+pablovi97@users.noreply.github.com>
2019-05-22 12:35:55 +0100

    comit
    comit1

---
+++ Cestacompra/src/main/java/modelo/Clientes.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author pablo
+ */
+@Entity
+@Table(name = "clientes")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Clientes.findAll", query = "SELECT c FROM Clientes c")
+    , @NamedQuery(name = "Clientes.findByIdCliente", query = "SELECT c FROM Clientes c WHERE c.idCliente = :idCliente")
+    , @NamedQuery(name = "Clientes.findByNombreCliente", query = "SELECT c FROM Clientes c WHERE c.nombreCliente = :nombreCliente")})
+public class Clientes implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id_cliente")
+    private Integer idCliente;
+    @Basic(optional = false)
+    @Column(name = "nombreCliente")
+    private String nombreCliente;
+
+    public Clientes() {
+    }
+
+    public Clientes(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Clientes(Integer idCliente, String nombreCliente) {
+        this.idCliente = idCliente;
+        this.nombreCliente = nombreCliente;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idCliente != null ? idCliente.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Clientes)) {
+            return false;
+        }
+        Clientes other = (Clientes) object;
+        if ((this.idCliente == null && other.idCliente != null) || (this.idCliente != null && !this.idCliente.equals(other.idCliente))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Clientes[ idCliente=" + idCliente + " ]";
+    }
+    
+}
---
+++ Cestacompra/src/main/java/modelo/Compra.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author pablo
+ */
+@Entity
+@Table(name = "compra")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Compra.findAll", query = "SELECT c FROM Compra c")
+    , @NamedQuery(name = "Compra.findByIdCompra", query = "SELECT c FROM Compra c WHERE c.idCompra = :idCompra")
+    , @NamedQuery(name = "Compra.findByNombreProducto", query = "SELECT c FROM Compra c WHERE c.nombreProducto = :nombreProducto")
+    , @NamedQuery(name = "Compra.findByCantidad", query = "SELECT c FROM Compra c WHERE c.cantidad = :cantidad")})
+public class Compra implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+    @Basic(optional = false)
+    @Column(name = "nombreProducto")
+    private String nombreProducto;
+    @Basic(optional = false)
+    @Column(name = "cantidad")
+    private int cantidad;
+    static int nuevaid=-1;
+
+    public Compra() {
+        nuevaid++;
+        this.idCompra=nuevaid;
+    }
+
+    public Compra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Compra(Integer idCompra, String nombreProducto, int cantidad) {
+        this.idCompra = idCompra;
+        this.nombreProducto = nombreProducto;
+        this.cantidad = cantidad;
+    }
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idCompra != null ? idCompra.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Compra)) {
+            return false;
+        }
+        Compra other = (Compra) object;
+        if ((this.idCompra == null && other.idCompra != null) || (this.idCompra != null && !this.idCompra.equals(other.idCompra))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto :"+ this.nombreProducto +" Cantidad :" + this.cantidad + " id_compra :" + this.idCompra;
+    }
+    
+}
